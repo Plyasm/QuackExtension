472b2af93f40bd82ac50b5eab3e850a03d775921
Plyasm <theplyasm@gmail.com>
2025-03-03 22:10:50 +0800

    first commit

---
+++ extension.js
@@ -0,0 +1,38 @@
+import { lib, game, ui, get, ai, _status } from "../../noname.js";
+export const type = "extension";
+export default function(){
+	return {name:"鸭子扩展",arenaReady:function(){
+    
+},content:function(config,pack){
+    
+},prepare:function(){
+    
+},precontent:function(){
+    
+},config:{},help:{},package:{
+    character: {
+        character: {
+        },
+        translate: {
+        },
+    },
+    card: {
+        card: {
+        },
+        translate: {
+        },
+        list: [],
+    },
+    skill: {
+        skill: {
+        },
+        translate: {
+        },
+    },
+    intro: "",
+    author: "Plyasm",
+    diskURL: "",
+    forumURL: "",
+    version: "1.0",
+},files:{"character":[],"card":[],"skill":[],"audio":[]},connect:false} 
+};
