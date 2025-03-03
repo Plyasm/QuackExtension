f8f23d7b99bdc0922f6aa65bd4a0193a266ac409
Plyasm <theplyasm@gmail.com>
2025-03-03 23:14:01 +0800

    加入武将和自定义势力代码

--- extension.js
+++ extension.js
@@ -8,12 +8,22 @@ export default function(){
 },prepare:function(){
     
 },precontent:function(){
-    
+    let empireConfig = {
+        color: '#160D08',
+    }
+    game.addGroup('empire', '帝', '帝国', empireConfig);
 },config:{},help:{},package:{
     character: {
         character: {
+            "quack_visha": ["female", "empire", 4/4, ["ys_yanfu","ys_shanzhan"],[
+                "des:维多莉亚·伊娃诺娃·谢列布里亚科夫少尉，又称维夏。谭雅的副官。",
+                "ext:鸭子扩展/image/character/quack_visha.jpg",
+                "die:ext:鸭子扩展/audio/die/quack_visha.mp3",
+                "forbidai"
+            ]],
         },
         translate: {
+            "quack_visha": "维多莉亚",
         },
     },
     card: {
